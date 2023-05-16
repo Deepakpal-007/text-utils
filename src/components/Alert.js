58ad453fc244c3edbb7f773d59c504397b3d3693
Deepakpal-007 <123586552+Deepakpal-007@users.noreply.github.com>
2023-05-16 18:48:48 +0530

    fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Alert(props) {
 
-    const capatalize = (word)=>{
-        const lower = word.toLowerCase()
-        return lower.charAt(0).toUpperCase() + lower.slice(1)
-    }
+  const capatalize = (word) => {
+    const lower = word.toLowerCase()
+    return lower.charAt(0).toUpperCase() + lower.slice(1)
+  }
 
 
   return (
     props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capatalize(props.alert.type)}</strong>: {props.alert.msg}
+      <strong>{capatalize(props.alert.type)}</strong>: {props.alert.msg}
     </div>
   )
 }
